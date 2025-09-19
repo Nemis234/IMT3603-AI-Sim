d5f69b29624d59dd51189b3e74bc569fe2436513
Bao Mark Ngyuen <baomn@stud.ntnu.no>
2025-09-19 13:38:16 +0200

    refactor: Fixed gameroot structure

--- Godot AI sim/ai-simulation-village/scenes/GameRoot/game_root.gd
+++ Godot AI sim/ai-simulation-village/scenes/GameRoot/game_root.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
+@onready var mainmenu: Control = $MainMenu
+
 func _ready():
 	
 	#Centers the Node to viewport
 	var viewport_center = get_viewport_rect().size / 2.0
 	position = viewport_center
+	
+	#Load the mainmenu upon start
+	load_and_switch_scene_from_path("res://scenes/UI/MainMenu/main_menu.tscn")
+	
+# Load the given scene
+func load_and_switch_scene_from_path(path: String):
+	get_tree().change_scene_to_file(path)
+
+	
+	
