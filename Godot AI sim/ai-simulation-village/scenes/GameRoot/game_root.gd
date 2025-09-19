0d8747c05923f93694272a9a790ad384bfe445be
Simen Hauger Wilberg <simenwilberg1@gmail.com>
2025-09-19 15:25:51 +0200

    fix: GameRoot node needing to move on start
    main_menu now contains a CanvasLayer so it is drawn independently of everything else.
    This scales everything in the menu, including the background, and positions it correctly.

--- Godot AI sim/ai-simulation-village/scenes/GameRoot/game_root.gd
+++ Godot AI sim/ai-simulation-village/scenes/GameRoot/game_root.gd
@@ -3,11 +3,6 @@ extends Node2D
 @onready var mainmenu: Control = $MainMenu
 
 func _ready():
-	
-	#Centers the Node to viewport
-	var viewport_center = get_viewport_rect().size / 2.0
-	position = viewport_center
-	
 	#Load the mainmenu upon start
 	load_and_switch_scene_from_path("res://scenes/UI/MainMenu/main_menu.tscn")
 	
