fdbbf756284ace242f6700941a75342c79c1ac0a
Bao Mark Ngyuen <baomn@stud.ntnu.no>
2025-09-22 21:49:20 +0200

    feat: Working on agent pathfinding with collisions

--- Godot AI sim/ai-simulation-village/scenes/Agent/agent.gd
+++ Godot AI sim/ai-simulation-village/scenes/Agent/agent.gd
@@ -8,6 +8,9 @@ extends CharacterBody2D
 #Testing purposes
 @export var move_to_target: Vector2
 
+#Pathfinding, check if something is blocking the agent on x-axis or y-axis
+var collision_axis: String = ""
+
 func _physics_process(delta: float) -> void:
 	#Decide target, target could be an entity in the game
 	#This way we could let the LLM decide where to go without querying every direction
@@ -18,7 +21,7 @@ func _physics_process(delta: float) -> void:
 	var move_dir = pathfinding_component.get_direction(global_position)
 	
 	#Feed the data from pathfinder_component into direction_component
-	direction_component.set_direction(move_dir)
+	direction_component.set_direction(move_dir, collision_axis)
 
 	#Update the velocity
 	velocity_component.accelerate_towards(direction_component.get_direction(), delta)
@@ -29,3 +32,19 @@ func _physics_process(delta: float) -> void:
 	
 	#Let the animation_component handle animations
 	animation_component.update_animation(velocity)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var dir = body.global_position - global_position
+	if abs(dir.x) > abs(dir.y):
+		# Horizontal collision
+		if dir.x > 0:
+			collision_axis = "X"
+	else:
+		# Vertical collision
+		if dir.y > 0:
+			collision_axis = "Y"
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	collision_axis = ""
--- Godot AI sim/ai-simulation-village/scenes/Components/direction_component.gd
+++ Godot AI sim/ai-simulation-village/scenes/Components/direction_component.gd
@@ -4,8 +4,15 @@ class_name DirectionComponent
 var direction: Vector2 = Vector2.ZERO
 
 #Usage example in another node: direction_component.set_direction(Vector2.RIGHT)
-func set_direction(dir: Vector2) -> void:
-	direction = dir.normalized()
+func set_direction(dir: Vector2, collision_dir: String) -> void:
+	#Check whenever agent collides with an object on the X- or Y-axis
+	match collision_dir:
+		"X":
+			direction = Vector2(0,1)
+		"Y":
+			direction = Vector2(1,0)
+		_:
+			direction = dir.normalized()
 
 func get_direction() -> Vector2:
 	return direction
