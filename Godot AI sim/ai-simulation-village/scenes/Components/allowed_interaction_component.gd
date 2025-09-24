1009962bf312f9580800137df16a1e1ab5abe849
Mark <baomn@stud.ntnu.no>
2025-09-24 11:07:54 +0200

    feat: add new components

---
+++ Godot AI sim/ai-simulation-village/scenes/Components/allowed_interaction_component.gd
@@ -0,0 +1,10 @@
+extends Node
+
+class_name AllowedInteractionComponent
+
+#This script will check if the enitity has met
+#all the conditions in order to interact with an another
+#entity
+
+func is_entity_within_range(entities: Array):
+	pass
