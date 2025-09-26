552d9b9bf194833acc1d4cb8620e0391df3a1c92
Bao Mark Ngyuen <baomn@stud.ntnu.no>
2025-09-26 03:29:15 +0200

    feat: #14 Player/Agents can interact with houses in the overworld to enter houses now

--- Godot AI sim/ai-simulation-village/scenes/Agentv2/agent_v_2.gd
+++ Godot AI sim/ai-simulation-village/scenes/Agentv2/agent_v_2.gd
@@ -5,10 +5,16 @@ extends CharacterBody2D
 @export var movement_speed = 5000
 @export var target: Vector2 
 
+#testing can delete later
+var this_guys_house
+func _ready() -> void:
+	for houses in get_tree().get_nodes_in_group("Houses_Main_Map"):
+		if houses.name == "HouseOverworld":
+			this_guys_house = houses
 
 func _physics_process(delta: float) -> void:
 	#TODO
-	navigationNode.target_position = target
+	navigationNode.target_position = this_guys_house.get_node("Area2D").get_global_position()
 	
 	if !navigationNode.is_target_reached():
 		var direciton = to_local(navigationNode.get_next_path_position()).normalized()
@@ -24,3 +30,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			for entities in detectionArea.get_overlapping_areas():
 				if entities.get_parent().is_in_group("interactable"):
 					entities.get_parent().change_state()
+				if entities.get_parent().is_in_group("Houses_Main_Map"):
+					self.position = entities.get_parent().entrance_position
+					navigationNode.target_position = entities.get_parent().entrance_position
+					print("Enters house")
