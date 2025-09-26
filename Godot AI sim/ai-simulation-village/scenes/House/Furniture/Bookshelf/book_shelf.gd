05928564a79a2ddc0b345fda82cc6334ea3db90d
Simen Hauger Wilberg <103248645+Nemis234@users.noreply.github.com>
2025-09-26 15:06:38 +0200

    Merge frontend ai (#15)
    * test: Add ollama test file
    
    * chore: adding the godot project from main, feat(main menu): adding a skeleton fo main menu
    
    * chore: added assets for maps and buildings
    
    * refactor: renamed folder, feat: Added a gameroot
    
    * feat: working on a prototype map
    
    * feat: added a prototype agent, still missing logic
    
    * feat: Added components for agent velocity,direction and pathfinding
    
    * feat: Added animation component to handle idle and walking animations
    
    * feat: Changed Main menu alignments and added Git plugin
    
    * chore: added .gitignore file
    
    * build: removed godot addons folder
    
    * refactor: Fixed gameroot structure
    
    * fix: Fixed pathfindingcomponent to not bounce the character upon reaching target, feat: Added proper loading sequence - Main menu -> LevelManager
    
    * chore: updated .gitignore
    
    * fix: GameRoot node needing to move on start
    
    main_menu now contains a CanvasLayer so it is drawn independently of everything else.
    This scales everything in the menu, including the background, and positions it correctly.
    
    * feature: added background image to title screen
    
    * feat: moved labels, changed button and label colors for title screen
    
    * chore: Added some interior assets
    
    * feat: Added a protype houseinterior, with 3 furnitures
    
    * feat: Working on agent pathfinding with collisions
    
    * Bug: changed Default Texture Filter to Nearest to fix blurry sprites
    
    * Feat: changed an interior object (bookshelf) to be StaticBody2D
    
    * feat: added furnitures
    
    * feat: add new components
    
    * build: update project version
    
    * feat: Add controllable player character with idle and walk states (see  #8)
    
    * chore: reimporting
    
    ---------
    
    Co-authored-by: rahul1epic <rahul1epic@gmail.com>
    Co-authored-by: Bao Mark Ngyuen <baomn@stud.ntnu.no>
    Co-authored-by: Christian Hegna Langedok <christian.hegna.langedok@gmail.com>

--- Godot AI sim/ai-simulation-village/scenes/House/Furniture/Bookshelf/book_shelf.gd
+++ Godot AI sim/ai-simulation-village/scenes/House/Furniture/Bookshelf/book_shelf.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+<<<<<<< HEAD
 @export var state_handler_component: StateHandlerComponent
 @onready var collisionArea: CollisionShape2D = $CollisionShape2D
 
@@ -7,3 +8,14 @@ extends StaticBody2D
 func change_state() -> void:
 	state_handler_component.change_state(0,collisionArea)
 	print("Interacted with Bookshelf")
+=======
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+>>>>>>> 0d6f73533fed8a39984c35caf85e172b0284ee21
