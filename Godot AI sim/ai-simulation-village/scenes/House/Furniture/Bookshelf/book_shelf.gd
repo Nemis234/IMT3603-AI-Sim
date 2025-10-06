d46d2843e283c520ac669df7b23cc44df8ab1fa7
rahul1epic <rahul1epic@gmail.com>
2025-10-06 16:43:02 +0530

    refactor: Remove old house structure

--- Godot AI sim/ai-simulation-village/scenes/House/Furniture/Bookshelf/book_shelf.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody2D
-
-@export var state_handler_component: StateHandlerComponent
-@onready var collisionArea: CollisionShape2D = $CollisionShape2D
-
-#Changes state
-func change_state() -> void:
-	state_handler_component.change_state(0,collisionArea)
-	print("Interacted with Bookshelf")
