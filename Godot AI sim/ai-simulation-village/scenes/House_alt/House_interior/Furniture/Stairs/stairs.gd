1bf3a1c298936d9fe06dc7d3fb2a1e1063d8337f
Christian Hegna Langedok <christian.hegna.langedok@gmail.com>
2025-10-06 11:51:01 +0200

    refactor: remove unused statments

--- Godot AI sim/ai-simulation-village/scenes/House_alt/House_interior/Furniture/Stairs/stairs.gd
+++ Godot AI sim/ai-simulation-village/scenes/House_alt/House_interior/Furniture/Stairs/stairs.gd
@@ -5,18 +5,18 @@ signal stair_used(target_floor: String)
 @export var target_stair: NodePath
 @export var target_floor: String
 
+# Stiars interact logic
 func interact(player):
+	# Set oposite stairs as target
 	var target = get_node_or_null(target_stair)
 	if target:
+		# Use position of CollistionShape2D to move player
 		var area = target.get_node_or_null("Area2D/CollisionShape2D")
-		print(area)
 		if area:
 			player.global_position = area.global_position
-		else:
-			player.global_position = target.global_position  # fallback
 		emit_signal("stair_used", target_floor)
 
-# To integrate with your LevelManager
+# To integrate with LevelManager
 func change_state():
 	var player = get_tree().get_first_node_in_group("Player")
 	if player:
--- Godot AI sim/ai-simulation-village/scenes/House_alt/House_interior/house_stairs.gd
+++ Godot AI sim/ai-simulation-village/scenes/House_alt/House_interior/house_stairs.gd
@@ -6,8 +6,8 @@ extends Node2D
 @onready var up_area: Area2D = stair_up.get_node("Area2D")
 @onready var down_area: Area2D = stair_down.get_node("Area2D")
 
+# Using same logic as entrance
 func _ready() -> void:
-	# Connect areas just like your doors
 	up_area.body_entered.connect(_on_area_entered.bind("up"))
 	up_area.body_exited.connect(_on_area_exited.bind("up"))
 
@@ -23,7 +23,7 @@ func _on_area_entered(body, area_name):
 
 func _on_area_exited(body, area_name):
 	if body.is_in_group("Player"):
-		# Check if still inside the other area
+		# Check if player still inside the other area
 		if area_name == "up" and down_area.get_overlapping_bodies().has(body):
 			body.curr_interactable = stair_down
 		elif area_name == "down" and up_area.get_overlapping_bodies().has(body):
