4e3e6b515c96399e484d7f30f4087c2ce90faa3d
Simen Hauger Wilberg <simenwilberg1@gmail.com>
2025-10-22 19:17:00 +0200

    Chore: gitignore only ignores godot-git-plugin in addons

--- Godot AI sim/ai-simulation-village/scripts/Server/test_server_basic.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@export var text_input : LineEdit
-@export var label_ : Label
-
-func _on_button_pressed() -> void:
-	var text = text_input.text
-	ServerConnection.post_message(text,label_)
-	
