763a2ecb632b926b2b595cb986a1c6be198634d0
Simen Hauger Wilberg <simenhw@stud.ntnu.no>
2025-10-24 13:58:02 +0200

    Fix: multiple requests clogging up the HTTP client

--- Godot AI sim/ai-simulation-village/scripts/Server/server_connection.gd
+++ Godot AI sim/ai-simulation-village/scripts/Server/server_connection.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var client : HTTPClient
+#var client : HTTPClient
 var stream = false
 
 
@@ -12,23 +12,28 @@ func _ready() -> void:
 	pass
 
 func connect_client():
+	var client
 	var err = 0
 	client = HTTPClient.new()
 	err = client.connect_to_host("http://127.0.0.1",8000)
 	assert(err==OK)
+	print("Connecting...")
 	while client.get_status() == HTTPClient.STATUS_CONNECTING or client.get_status() == HTTPClient.STATUS_RESOLVING:
 		client.poll()
-		print("Connecting...")
 		await get_tree().process_frame
 	
 	assert(client.get_status() == HTTPClient.STATUS_CONNECTED)
-
+	
+	return client
+	
 ## Recicpiant is the AI agent that is being talked too. 
 ## Will be an ENUM with all available agents. 
 ## Requests a spesific agent using its url [code]"/chat/{recipiant}"[/code] [br]
 ## Participant is whoever is talking to the AI agent.
 ## Defaults to "user" [br]
 func post_message(agentName:String,message:String, label_:Label, type:String ="chat", participant="user", recipiant:int=0):
+	var client = await connect_client()
+	
 	var err = 0
 	var fields = {
 		"agent":agentName,
@@ -46,14 +51,14 @@ func post_message(agentName:String,message:String, label_:Label, type:String ="c
 	client.poll()
 	if client.get_status() == HTTPClient.STATUS_CONNECTION_ERROR:
 		await connect_client()
-	
+	var status = client.get_status()
 	err = client.request(HTTPClient.METHOD_POST,"/"+type,headers,query_string)
 	assert(err == OK)
 	
+	print("Requesting...")
 	while client.get_status() == HTTPClient.STATUS_REQUESTING:
 		# Keep polling for as long as the request is being processed.
 		client.poll()
-		print("Requesting...")
 		await get_tree().process_frame
 	
 	assert(client.get_status() == HTTPClient.STATUS_BODY or client.get_status() == HTTPClient.STATUS_CONNECTED)
