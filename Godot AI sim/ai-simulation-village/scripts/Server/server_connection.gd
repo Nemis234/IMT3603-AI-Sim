c01ed6e83cfd3859fdfb03c564403635ec382720
Simen Hauger Wilberg <simenwilberg1@gmail.com>
2025-10-26 14:42:31 +0100

    Refactor:  server_connection returns the response, made code more modular

--- Godot AI sim/ai-simulation-village/scripts/Server/server_connection.gd
+++ Godot AI sim/ai-simulation-village/scripts/Server/server_connection.gd
@@ -1,9 +1,8 @@
 extends Node
 
-#var client : HTTPClient
-var stream = false
 
-
+const SERVER_URL = "http://127.0.0.1"
+const SERVER_PORT = 8000
 
 
 # I initialise the request
@@ -11,11 +10,11 @@ func _ready() -> void:
 	#connect_client()
 	pass
 
-func connect_client():
+func connect_client() -> HTTPClient:
 	var client
 	var err = 0
 	client = HTTPClient.new()
-	err = client.connect_to_host("http://127.0.0.1",8000)
+	err = client.connect_to_host(SERVER_URL,SERVER_PORT)
 	assert(err==OK)
 	print("Connecting...")
 	while client.get_status() == HTTPClient.STATUS_CONNECTING or client.get_status() == HTTPClient.STATUS_RESOLVING:
@@ -25,16 +24,49 @@ func connect_client():
 	assert(client.get_status() == HTTPClient.STATUS_CONNECTED)
 	
 	return client
+
+
+## Sends a request using the given client. [br]
+## Output text is written to the label_, and is returned in full when complete. [br]
+func send_request(label_:Label,client:HTTPClient,method:HTTPClient.Method,url:String,headers:PackedStringArray,query_string:String="")->String:
+	client.poll()
+	if client.get_status() == HTTPClient.STATUS_CONNECTION_ERROR:
+		await connect_client()
 	
-## Recicpiant is the AI agent that is being talked too. 
-## Will be an ENUM with all available agents. 
-## Requests a spesific agent using its url [code]"/chat/{recipiant}"[/code] [br]
+	var status = client.get_status()
+	var err = client.request(method,url,headers,query_string)
+	assert(err == OK)
+	
+	print("Requesting to '",url,"'...")
+	while client.get_status() == HTTPClient.STATUS_REQUESTING:
+		# Keep polling for as long as the request is being processed.
+		client.poll()
+		await get_tree().process_frame
+	
+	assert(client.get_status() == HTTPClient.STATUS_BODY or client.get_status() == HTTPClient.STATUS_CONNECTED)
+	
+	var rb = PackedByteArray() # Array that will hold the data.
+	while client.get_status() == HTTPClient.STATUS_BODY:
+		# While there is body left to be read
+		client.poll()
+		# Get a chunk.
+		var chunk = client.read_response_body_chunk()
+		if chunk.size() == 0:
+			await get_tree().process_frame
+		else:
+			rb = rb + chunk # Append to read buffer.
+			label_.text = rb.get_string_from_utf8()
+			#print(rb.get_string_from_utf8())
+	print("bytes got: ", rb.size())
+	var text = rb.get_string_from_utf8()
+	return text
+
+
 ## Participant is whoever is talking to the AI agent.
 ## Defaults to "user" [br]
-func post_message(agentName:String,message:String, label_:Label, type:String ="chat", participant="user", recipiant:int=0):
-	var client = await connect_client()
+func post_message(agentName:String,message:String, label_:Label, type:String="chat", participant="user")->String:
+	var client := await connect_client()
 	
-	var err = 0
 	var fields = {
 		"agent":agentName,
 		"time":str(Global.hour) + ":" + str(Global.minute),
@@ -48,42 +80,14 @@ func post_message(agentName:String,message:String, label_:Label, type:String ="c
 		"Accept: */*"
 	]
 	
-	client.poll()
-	if client.get_status() == HTTPClient.STATUS_CONNECTION_ERROR:
-		await connect_client()
-	var status = client.get_status()
-	err = client.request(HTTPClient.METHOD_POST,"/"+type,headers,query_string)
-	assert(err == OK)
+	var text = await send_request(label_,client,HTTPClient.METHOD_POST,"/"+type,headers,query_string)
 	
-	print("Requesting...")
-	while client.get_status() == HTTPClient.STATUS_REQUESTING:
-		# Keep polling for as long as the request is being processed.
-		client.poll()
-		await get_tree().process_frame
-	
-	assert(client.get_status() == HTTPClient.STATUS_BODY or client.get_status() == HTTPClient.STATUS_CONNECTED)
-	
-	var rb = PackedByteArray() # Array that will hold the data.
-	while client.get_status() == HTTPClient.STATUS_BODY:
-		# While there is body left to be read
-		client.poll()
-		# Get a chunk.
-		var chunk = client.read_response_body_chunk()
-		if chunk.size() == 0:
-			await get_tree().process_frame
-		else:
-			rb = rb + chunk # Append to read buffer.
-			label_.text = rb.get_string_from_utf8()
-			#print(rb.get_string_from_utf8())
-	print("bytes got: ", rb.size())
-	var text = rb.get_string_from_utf8()
-	print("Text: ", text)
+	return text
 	
 
 #New creating sepearte post method for actions (passing entire dict of agent details so it becomes easier to add more details)
 func post_action(agent_details:Dictionary, label_:Label):
 	var client = await connect_client()
-	var err = 0
 	print(agent_details)
 	
 	var query_string = JSON.stringify(agent_details)
@@ -92,32 +96,6 @@ func post_action(agent_details:Dictionary, label_:Label):
 		"Accept: */*"
 	]
 	
-	client.poll()
-	if client.get_status() == HTTPClient.STATUS_CONNECTION_ERROR:
-		await connect_client()
+	var text = await send_request(label_,client,HTTPClient.METHOD_POST,"/action",headers,query_string)
+	print(text)
 	
-	err = client.request(HTTPClient.METHOD_POST,"/action",headers,query_string)
-	assert(err == OK)
-	
-	while client.get_status() == HTTPClient.STATUS_REQUESTING:
-		# Keep polling for as long as the request is being processed.
-		client.poll()
-		await get_tree().process_frame
-	
-	assert(client.get_status() == HTTPClient.STATUS_BODY or client.get_status() == HTTPClient.STATUS_CONNECTED)
-	
-	var rb = PackedByteArray() # Array that will hold the data.
-	while client.get_status() == HTTPClient.STATUS_BODY:
-		# While there is body left to be read
-		client.poll()
-		# Get a chunk.
-		var chunk = client.read_response_body_chunk()
-		if chunk.size() == 0:
-			await get_tree().process_frame
-		else:
-			rb = rb + chunk # Append to read buffer.
-			label_.text = rb.get_string_from_utf8()
-			#print(rb.get_string_from_utf8())
-	print("bytes got: ", rb.size())
-	var text = rb.get_string_from_utf8()
-	print("Text: ", text)
