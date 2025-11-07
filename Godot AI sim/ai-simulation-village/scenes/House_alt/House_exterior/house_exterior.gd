ff0971ea41869bcfe9e7ecc8d1c748da90315633
Christian Hegna Langedok <christian.hegna.langedok@gmail.com>
2025-11-07 16:02:28 +0100

    feat: update House3 structure in LevelManager

--- Godot AI sim/ai-simulation-village/scenes/House_alt/House_exterior/house_exterior.gd
+++ Godot AI sim/ai-simulation-village/scenes/House_alt/House_exterior/house_exterior.gd
@@ -3,9 +3,9 @@ extends interactable
 @onready var house = self.get_parent()
 
 func _ready() -> void:
-    interact_area = $Entrance #Redeclaring intercat area cause Area2D name is different for houses
-    super()
+	interact_area = $Entrance #Redeclaring intercat area cause Area2D name is different for houses
+	super()
 
 func change_state(node:Node)->void:
-    if node and node.is_in_group("Player"):
-        node.position = house.exit_area.get_global_position() #Let player enter house
+	if node and node.is_in_group("Player"):
+		node.position = house.exit_area.get_global_position() #Let player enter house
