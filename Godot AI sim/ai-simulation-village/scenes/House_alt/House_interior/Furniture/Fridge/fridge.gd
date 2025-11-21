2cb4b176bda3840f0de98ab7a029f054542636fa
Christian Hegna Langedok <christian.hegna.langedok@gmail.com>
2025-11-21 13:54:45 +0100

    feat: add inventory to fridge

--- Godot AI sim/ai-simulation-village/scenes/House_alt/House_interior/Furniture/Fridge/fridge.gd
+++ Godot AI sim/ai-simulation-village/scenes/House_alt/House_interior/Furniture/Fridge/fridge.gd
@@ -2,10 +2,18 @@ extends interactable
 
 @export var state_handler_component: StateHandlerComponent
 @onready var collisionArea: CollisionShape2D = $CollisionShape2D
-signal request_popup(question: String, choises: Array)
+@onready var inventory: Inventory = $Inventory
+
+signal open_inventory(label_text: String, inventory: Array[Item], source: Node, mode: String)
+
+func fill_fridge() -> void:
+	inventory.add_item("Egg", 6)
+	inventory.add_item("Bacon", 3)
+	inventory.remove_item("Bacon", 2)
 
 #Changes state
 func change_state(node: Node) -> void:
 	if node.is_in_group("Player"):
 		node.in_interaction = true
-		emit_signal("request_popup", "What would you like to take from the fridge?", ["Milk", "Egg", "Cheese", "Nothing"])
+		node.curr_interactable = self
+		emit_signal("open_inventory", "Fridge inventory", inventory.items)
