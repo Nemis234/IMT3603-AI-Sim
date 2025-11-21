bc97143c97f87b13cd50c751a5cc714f30d701f8
Christian Hegna Langedok <christian.hegna.langedok@gmail.com>
2025-11-21 13:48:46 +0100

    feat: add use item

--- Godot AI sim/ai-simulation-village/scripts/item.gd
+++ Godot AI sim/ai-simulation-village/scripts/item.gd
@@ -1,10 +1,19 @@
+extends Resource
 class_name  Item
 
-var name: String 
-var description: String
-var quantity: int
+@export var name: String
+@export var description: String
+@export var is_usable: bool
+@export var quantity: int = 1
 
-func _init(item_name: String, item_description: String ="", item_quantity: int = 1):
+func _init(item_name: String = "", item_description: String = "", item_is_usable: bool = false, item_quantity: int = 1):
 	self.name = item_name
 	self.description = item_description
+	self.is_usable = item_is_usable
 	self.quantity = item_quantity
+
+func add_quantity(amount: int):
+	quantity += amount
+	
+func remove_quantity(amount: int):
+	quantity = max(quantity - amount, 0)
