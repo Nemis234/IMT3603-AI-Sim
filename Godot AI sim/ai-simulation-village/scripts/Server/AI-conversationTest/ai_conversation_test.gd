c5e2f10554f27ae69d268f49bdbb1d8828e2fc57
Nemis234 <simenhw@stud.ntnu.no>
2025-12-13 21:54:17 +0100

    Bug: fixed invalid UUIDs, and missing walkable tiles map

--- Godot AI sim/ai-simulation-village/scripts/Server/AI-conversationTest/ai_conversation_test.gd
+++ Godot AI sim/ai-simulation-village/scripts/Server/AI-conversationTest/ai_conversation_test.gd
@@ -2,20 +2,23 @@ extends Control
 
 @onready var chat1: Label = $VBoxContainer/ChatBubble1/Chat
 @onready var chat2: Label = $VBoxContainer/ChatBubble2/Chat
-@onready var agent_1: Label = $VBoxContainer/ChatBubble1/Agent_1
-@onready var agent_2: Label = $VBoxContainer/ChatBubble2/Agent_2
 
+@onready var name_label1: Label = $VBoxContainer/SpeechBubble/PanelContainer/VBoxContainer/NameLabel
+@onready var name_label2: Label = $VBoxContainer/SpeechBubble2/PanelContainer/VBoxContainer/NameLabel
+
+@onready var speech_bubble: SpeechBubble = $VBoxContainer/SpeechBubble
+@onready var speech_bubble_2: SpeechBubble = $VBoxContainer/SpeechBubble2
 
 
 var agent1 = "John"
 var agent2 = "Mei"
 
 func _ready() -> void:
-	agent_1.text = agent1 + ":"
-	agent_2.text = agent2 + ":"
+	name_label1.text = agent1 + ":"
+	name_label2.text = agent2 + ":"
 
 func _on_button_pressed() -> void:
-	AiConversation.init_agent2agent_conversation(agent1,agent2,chat1,chat2)
+	AiConversation.init_agent2agent_conversation(agent1,agent2,speech_bubble,speech_bubble_2)
 
 
 func _on_button_2_pressed() -> void:
